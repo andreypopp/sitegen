ecd640e2455d8f390fb4d97b746756c1a7edd4bf
Andrey Popp <8mayday@gmail.com>
2015-09-26 02:21:24 +0500

    refactor(link-api): remove PageLink dead code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 export Site               from './Site';
 export createPage         from './createPage';
 export createSite         from './createSite';
-export PageLink           from './PageLink';
 export * as LinkRegistry  from './LinkRegistry';
