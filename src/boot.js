37eb2329d6b4503a3683fae23836c1df9934d98b
Andrey Popp <8mayday@gmail.com>
2016-05-18 02:53:15 +0300

    fix(route): recreate router if it cannot handle the update

--- src/boot.js
+++ src/boot.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import makeDebug from 'debug';
 import {render} from 'react-dom'
-import {browserHistory, Router, match} from 'react-router'
+import {browserHistory, Router as RouterBase, match} from 'react-router'
 import {AppContainer} from 'react-hot-loader';
 
 makeDebug.enable(__DEBUG__);
 
 let debug = makeDebug('sitegen:runtime:boot');
+
+class Router extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {key: 0};
+  }
+
+  render() {
+    return <RouterBase key={this.state.key} {...this.props} />;
+  }
+
+  componentWillReceiveProps({routes}) {
+    if (this.props.routes !== routes) {
+      this.setState({key: this.state.key + 1});
+    }
+  }
+}
 
 export function boot(routes) {
   debug('matching');
