2832ed4a444850bb74d18dc44280592effeaeaf8
Andrey Popp <8mayday@gmail.com>
2016-05-17 23:21:43 +0300

    feat(meta): initial take on loader

--- src/loader/meta.js
+++ src/loader/meta.js
@@ -1,3 +1,34 @@
-module.exports = function(source) {
-  return `module.exports.default = {meta: "${this.resource}"};`;
+import {parse} from 'babylon';
+import traverse from 'babel-traverse';
+import generate from 'babel-generator';
+import * as types from 'babel-types';
+
+module.exports = function meta(source) {
+  this.cacheable();
+
+  let found = null;
+
+  traverse(parse(source), {
+    enter(path) {
+      if (types.isAssignmentExpression(path.node) && isExportsMeta(path.node.left)) {
+        found = path.node.right;
+      }
+    }
+  });
+
+  if (found) {
+    return `module.exports = ${generate(found).code};`;
+  } else {
+    return `module.exports.default = {}`;
+  }
+}
+
+function isExportsMeta(node) {
+  return (
+    types.isMemberExpression(node) &&
+    types.isIdentifier(node.object) &&
+    node.object.name === 'exports' &&
+    types.isIdentifier(node.property) &&
+    node.property.name === 'meta'
+  );
 }
