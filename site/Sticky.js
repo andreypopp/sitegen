694a547d67b2c391aa4408034062d20ff46140c2
Andrey Popp <8mayday@gmail.com>
2016-05-22 15:07:41 +0300

    doc: rework sticky nav

--- site/Sticky.js
+++ site/Sticky.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import {topTop, calculateScrollY} from 'react-track/lib/tracking-formulas';
+import {topTop, calculateScrollY, isMeasured} from 'react-track/tracking-formulas';
 import {TrackDocument, TrackedDiv} from 'react-track';
 import {Gateway, GatewayDest, GatewayProvider} from 'react-gateway';
 
 export const GATEWAY_NAME = "fixedTop";
 
 export function Sticky({children}) {
+
+  function renderChildren(scrollY, top, isMeasured) {
+    let stickyChildren = React.cloneElement(children, {
+      sticky: true,
+      stickyActive: isMeasured && top > 0 && scrollY >= 0,
+    });
+    return (
+      <div>
+        <Gateway into={GATEWAY_NAME}>
+          {stickyChildren}
+        </Gateway>
+        {children}
+      </div>
+    );
+  }
+
   return (
-    <TrackDocument formulas={[topTop]}>
+    <TrackDocument updateOnDidMount formulas={[topTop]}>
       {topTop =>
-        <TrackedDiv formulas={[calculateScrollY, topTop]}>
-          {(scrollY, top) =>
-            <div>
-              {top > 0 && scrollY > 0 &&
-                <Gateway into={GATEWAY_NAME}>
-                  {React.cloneElement(children, {sticky: true})}
-                </Gateway>}
-              {children}
-            </div>}
+        <TrackedDiv formulas={[calculateScrollY, topTop, isMeasured]}>
+          {renderChildren}
         </TrackedDiv>}
     </TrackDocument>
   );
