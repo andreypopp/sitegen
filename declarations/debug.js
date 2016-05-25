405ec005f4fbcc638142f2b7ca2c50d619d1b27d
Andrey Popp <8mayday@gmail.com>
2016-05-25 18:53:00 +0300

    fix(declarations): fixes

--- declarations/debug.js
+++ declarations/debug.js
@@ -1,4 +1,4 @@
-declare module "debug" {
+declare module 'debug' {
   declare type Logger = (msg: string) => void;
   declare type LoggerFactory = (msg: string) => Logger;
   declare module.exports: LoggerFactory;
--- declarations/webpack.js
+++ declarations/webpack.js
@@ -1,16 +1,28 @@
 declare module 'webpack' {
+
+  declare class UglifyJsPlugin {
+
+    constructor(params: mixed): UglifyJsPlugin;
+  }
+
+  declare var optimize: {
+    UglifyJsPlugin: typeof UglifyJsPlugin;
+  };
+
   declare type Stats = {
     compilation: Compilation;
+    [key: string]: mixed;
   };
 
   declare type Compilation = {
     errors: Array<Error>;
     warnings: Array<Error>;
+    [key: string]: mixed;
   };
 
   declare type Compiler = {
-    debug?: Logger;
     plugin(name: string, callback: Function): void;
+    [key: string]: mixed;
   };
 
   declare type Asset
